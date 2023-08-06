d68a789a319b568f593f0a799f61d8f968464662
Andrew <papsujevicandrew@gmail.com>
2023-08-06 19:29:09 +0300

    hw2 ready
    Ready for review hw2

---
+++ schemas/contactsSchema.js
@@ -0,0 +1,9 @@
+const Joi = require("joi");
+
+const addSchema = Joi.object({
+  name: Joi.string().trim().min(2).max(30).required(),
+  email: Joi.string().required(),
+  phone: Joi.string().trim().required(),
+});
+
+module.exports = { addSchema };
